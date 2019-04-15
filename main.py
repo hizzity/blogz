95c62c4cc70c1d19efd780701f86e18e2a8f707a
Katie Brown <hizzity@gmail.com>
2019-04-15 13:18:19 -0500

    progress

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
+#import jinja2
+
+#template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+#jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -12,36 +17,43 @@ class Blog(db.Model):
     title = db.Column(db.String(200))
     entry = db.Column(db.String(1000))
 
-    def __init__(self, title, entry): ##?? 
+    def __init__(self, title, entry): ##constructor 
         self.title = title
         self.entry = entry
 
 
-@app.route('/newpost')
-def blogpost():
-    title = request.form['title']
-    entry = request.form['entry']
-    if title == '' or entry == '':
-        redirect('/blog')
-    else:
-        new_title = Blog(title)
-        new_entry = Blog(entry)
-        db.sessions.add(new_title, new_task) ###how to enter info into table?
-        db.session.commit()
+#@app.route('/newpost') 
+#def newpost():
+#    title = request.form['title']
+#    entry = request.form['entry']
+#    if len(title) == 0 or len(entry) == 0:
+#         redirect('/blog')
+#    else:
+#        new_title = Blog(title)
+#        new_entry = Blog(entry)
+#        db.sessions.add(new_title, new_task) ###how to enter mulitiply cols of info into table?
+#        db.session.commit()  
 
-       
+#    return render_template("blog.html")
 
-    return render_template("newpost.html")
+#@app.route('/single_blog')
+# when click on a blog, get id number, use that to display the title and entry
+#def one_entry():
+    #get title from the data table title = blog[i].title
+    #get entry from the data table entry = blog[i].entry
+    # return rendered_template("single_blog.html", title=title, entry=entry)
 
     
 
-@app.route('/blog')
-def index():
-    blog_title = request.form['title']
-    entry = request.form['entry']
-
-
-    return render_template("blog.html")
+@app.route('/') # should it be '/blog'? no bc function invokes blog.html...I think?
+def blog():
+    #title = request.form['title']
+    #entry = request.form['entry']
+    #template = jinja_env.get_template('blog.html')
+    #return template.render()   ## lines 43-46 from class 6 prep on jinja2
+    
+    blogs = Blog.query.all()
+    return render_template("blog.html", title="Blog", blogs=blogs) #if not using jinja
 
 if __name__ == '__main__':
     app.run()
