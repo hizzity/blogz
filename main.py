f10fbe665362a10287604d0cdf9df9cd70f8d360
Katie Brown <hizzity@gmail.com>
2019-04-18 20:08:39 -0500

    links are working! single page is working! commiting is working!

--- main.py
+++ main.py
@@ -20,31 +20,32 @@ class Blog(db.Model):
     def __str__(self):
         return '<Blog {0}>'.format(self.title)  ##to see in python shell? not nec to run site
 
-@app.route('/<blog_id>')  #blog_id in path? @app.route("/data/<section>") def data(section): assert section == request.view_args['section']
+@app.route('/?id=Blog.id')  #blog_id in path? @app.route("/data/<section>") def data(section): assert section == request.view_args['section']
 def one_post(blog_id):
-    assert blog_id == int(request.form['title']  #'title' would be the one 'clicked' on
-    title = Blog.query.get(blog_id)   #TODO what goes in ()
-    entry = Blog.query.get(blog_id)   #TODO what goes in ()
-    return render_template('single_blog.html', title=title, entry=entry)
-
-
-@app.route('/new', methods=['POST','GET']) 
-def newpost():
-    return render_template('/newpost.html')
-    #TODO route or redirect to /single_blog.html
     
-@app.route('/blog', methods=['POST','GET']) 
-def index():
-    #this should add any new entries to the list of blogs...*should*
-    if request.method == 'POST':
-        new_title = request.form['new_title']
-        new_entry = request.form['new_entry']
-        db.session.add(new_title, new_entry)
+    return render_template('single_blog', title=title, entry=entry)
+
+
+@app.route('/newpost', methods=['POST','GET']) 
+def newpost():
+    if request.method == 'GET':           #from today in class 4/18
+        return render_template('/newpost.html')
+    else:
+        #passes info from new post to /single_blog and commits the new blog to the database
+        title = request.form['new_title']
+        entry = request.form['new_entry']
+        db.session.add(Blog(title,entry))
         db.session.commit()
-        
+    return render_template('/single_blog.html', title=title, entry=entry)
+ #TODO test this function
+    
+@app.route('/') 
+def index():     
     blogs = Blog.query.all()  # Blog.query.get(new_title) to get id of the new_title 
-    return render_template("blog.html", blogs=blogs)
+    return render_template("/blog.html", blogs=blogs)
 
 
 if __name__ == '__main__':
     app.run()
+
+    
