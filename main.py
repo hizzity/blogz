1f57dea6ee420cd9ebe73f5b88b41b7df4d587fc
Katie Brown <hizzity@gmail.com>
2019-04-22 19:02:59 -0500

    It's FINISHEDgit add .

--- main.py
+++ main.py
@@ -20,36 +20,36 @@ class Blog(db.Model):
     def __str__(self):
         return '<Blog {0}>'.format(self.title)  ##to see in python shell? not nec to run site
 
-@app.route('/<blog_id>')  # ?id= blog_id in path? @app.route("/data/<section>") def data(section): assert section == request.view_args['section']
-def one_post(blog_id):
-    blog_id = request.form['blog_id']          ## blog_id from hidden entry during for loop in index.html
-    title = session.query(Blog).get('blog_id')  ## TODO not sure if syntax is correct, getting 404 erro
-    entry = session.query(Blog).get('blog_id')
-    return render_template('single_blog', title=title, entry=entry)
+@app.route('/single_blog')  # ?id= blog_id in path?
+def one_post():
+    blog_id = request.args.get('id')      
+    blog = Blog.query.get(blog_id) 
+    return render_template('single_blog.html', blog=blog)
 
 
 @app.route('/newpost', methods=['POST','GET']) 
 def newpost():
-    if request.method == 'GET':           #from today in class 4/18
+    if request.method == 'GET':           #from class 4/18
         return render_template('/newpost.html')
     else:
         title = request.form['new_title']
         entry = request.form['new_entry']
         if title == '' and entry == '':
             error = "Please enter a title."
-            error_entry = "The blog entry can not be left blank"
-            return render_template('/newpost.html', error=error, error_entry=error_entry)
-        if title == '':
+            entry_error = "The blog entry can not be left blank"
+            return render_template('/newpost.html', error=error, entry_error=entry_error)
+        elif title == '':
             error = "Please enter a title."
             return render_template('/newpost.html', error=error)
-        if entry == '':
+        elif entry == '':
             entry_error = "The blog entry can not be left blank"
             return render_template('/newpost.html', entry_error=entry_error)
         else:
             #passes info from new post to /single_blog and commits the new blog to the database
-            db.session.add(Blog(title,entry))
+            blog = Blog(title,entry)
+            db.session.add(blog)
             db.session.commit()
-        return render_template('/single_blog.html', title=title, entry=entry)
+        return render_template('/single_blog.html', blog=blog)
  
     
 @app.route('/') 
