0ed064c98dcaf59c5dc137889f777079630aae6c
Katie Brown <hizzity@gmail.com>
2019-04-22 19:32:42 -0500

    MAMP issues, had to change the port back to deflault

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:launchcode@localhost:8891/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:launchcode@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
